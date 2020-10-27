27b37496d39035fdad460010a450f21f582c8212
azymberdi <57076637+azymberdi@users.noreply.github.com>
2020-10-26 22:22:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -118,3 +118,28 @@ resource "aws_security_group" "allow" {
 variable "access_key" {}
 variable "secret_key" {}
 #variable "aws_region" {}
+variable "tag" {}
+
+# # Public Subnet
+# ########################################################
+resource "aws_subnet" "public1" {
+    vpc_id     = "${aws_vpc.main.id}"
+    cidr_block = "${var.public1_cidr}"
+    availability_zone = "${var.region}${var.az1}"
+    map_public_ip_on_launch = true
+    tags = "${var.tags}"
+}
+resource "aws_subnet" "public2" {
+    vpc_id     = "${aws_vpc.main.id}"
+    cidr_block = "${var.public2_cidr}"
+    availability_zone = "${var.region}${var.az2}"
+    map_public_ip_on_launch = true
+    tags = "${var.tags}"
+}
+resource "aws_subnet" "public3" {
+    vpc_id     = "${aws_vpc.main.id}"
+    cidr_block = "${var.public3_cidr}"
+    availability_zone = "${var.region}${var.az3}"
+    map_public_ip_on_launch = true
+    tags = "${var.tags}"
+}
