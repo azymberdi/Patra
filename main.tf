7dde2fe657f38f839e4400268147e47873028645
azymberdi <57076637+azymberdi@users.noreply.github.com>
2020-10-26 23:04:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -133,19 +133,16 @@ resource "aws_subnet" "public1" {
     cidr_block = "10.0.101.0/24"
     availability_zone = "us-west-2a"
     map_public_ip_on_launch = true
-    tags = "${var.tags}"
 }
 resource "aws_subnet" "public2" {
     vpc_id     = "${aws_vpc.main.id}"
     cidr_block = "10.0.102.0/24"
     availability_zone = "us-west-2b"
     map_public_ip_on_launch = true
-    tags = "${var.tags}"
 }
 resource "aws_subnet" "public3" {
     vpc_id     = "${aws_vpc.main.id}"
     cidr_block = "10.0.103.0/24"
     availability_zone = "us-west-2c"
     map_public_ip_on_launch = true
-    tags = "${var.tags}"
 }
