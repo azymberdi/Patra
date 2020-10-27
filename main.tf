5045de69a8e57370df28d8ecfd81bcae4f11bb4e
azymberdi <57076637+azymberdi@users.noreply.github.com>
2020-10-26 22:07:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ launch_template  {
 
 
 resource "aws_security_group" "allow" {
-  name        = "allowitforPatra"
+  name        = "PatraallowitforPatra"
   description = "Allow TLS inbound traffic"
   vpc_id      = "vpc-0e310f83d05bd890a"
 
