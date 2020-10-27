946492b9fa50755207408f4f5efd100989e7fd7e
azymberdi <57076637+azymberdi@users.noreply.github.com>
2020-10-26 22:44:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -130,21 +130,21 @@ variable "region" {}
 # ########################################################
 resource "aws_subnet" "public1" {
     vpc_id     = "${aws_vpc.main.id}"
-    cidr_block = "${var.public1_cidr}"
+    cidr_block = ""10.0.101.0/24""
     availability_zone = "${var.region}a"
     map_public_ip_on_launch = true
     tags = "${var.tags}"
 }
 resource "aws_subnet" "public2" {
     vpc_id     = "${aws_vpc.main.id}"
-    cidr_block = "${var.public2_cidr}"
+    cidr_block = ""10.0.102.0/24""
     availability_zone = "${var.region}b"
     map_public_ip_on_launch = true
     tags = "${var.tags}"
 }
 resource "aws_subnet" "public3" {
     vpc_id     = "${aws_vpc.main.id}"
-    cidr_block = "${var.public3_cidr}"
+    cidr_block = ""10.0.103.0/24""
     availability_zone = "${var.region}c"
     map_public_ip_on_launch = true
     tags = "${var.tags}"
